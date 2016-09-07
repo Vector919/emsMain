097798880ad662c6bf86a268666aa6b6dd1f096e
awm4j <awebmaster4jesus@gmail.com>
2016-09-07 19:42:08 -0400

    updated the moveReplaceAPK.bat

--- moveReplaceAPK.bat
+++ moveReplaceAPK.bat
@@ -1,1 +1,3 @@
-copy /y platforms\android\build\outputs\apk\android-debug.apk hashtagems.apk
+copy /y platforms\android\build\outputs\apk\android-debug.apk hashtagems-debug.apk
+copy /y platforms\android\build\outputs\apk\android-release-unsigned.apk hashtagems-release-unsigned.apk
+
